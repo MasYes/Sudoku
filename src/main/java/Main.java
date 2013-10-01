1de5d149093cd71e4fd0996b2025f53ee79bf41c
MasYes <bymasyes@gmail.com>
2013-10-01 16:47:20 +0400

    Now it's better, faster, stronger.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,51 +6,41 @@
  * To change this template use File | Settings | File Templates.
  */
 import java.util.HashSet;
+import java.util.HashMap;
+import java.io.FileInputStream;
+import java.util.Scanner;
 
 public class Main {
+
 	public static void main(String[] args) throws Exception{
-		int[][] matrix = new int[9][9];
+		System.out.println(isValid("sudoku.txt"));
+	}
 
-		String matrixS =
-						"1 2 3 4 5 6 7 8 9" +
-						" 7 8 9 1 2 3 4 5 6" +
-						" 4 5 6 7 8 9 1 2 3" +
-						" 9 1 2 3 4 5 6 7 8" +
-						" 6 7 8 9 1 2 3 4 5" +
-						" 3 4 5 6 7 8 9 1 2" +
-						" 8 9 1 2 3 4 5 6 7" +
-						" 5 6 7 8 9 1 2 3 4" +
-						" 2 3 4 5 6 7 8 9 1" ;
-
+	private static boolean isValid(String file) throws Exception{
+		Scanner scan = new Scanner(new FileInputStream(file));
+		HashMap<Integer, HashSet<Integer>> rows = new HashMap<>();
+		HashMap<Integer, HashSet<Integer>> columns = new HashMap<>();
+		HashMap<Integer, HashSet<Integer>> areas = new HashMap<>();
 		int count = 0;
-		for(String i : matrixS.split(" ")){
-			matrix[count/9][count%9] = Integer.parseInt(i);
+		for(int i = 0; i < 9; i++){
+			rows.put(i, new HashSet<Integer>());
+			columns.put(i, new HashSet<Integer>());
+			areas.put(i, new HashSet<Integer>());
+		}
+		int i;
+		while(scan.hasNextInt()){
+			i = scan.nextInt();
+			if(!rows.get(count/9).add(i)){
+				return false;
+			}
+			if(!columns.get(count % 9).add(i)){
+				return false;
+			}
+			if(!areas.get(((count/9)/3)*3 + (count%9)/3).add(i)){
+				return false;
+			}
 			count++;
 		}
-
-		boolean ok = true;
-		HashSet<Couple> set;
-		HashSet<Integer> strings;
-		HashSet<Integer> columns;
-		for(int i = 1; i < 10; i++){
-			strings = new HashSet<>();
-			set = new HashSet<>();
-			columns = new HashSet<>();
-			for(count = 0; count < 9*9; count++){
-				if(matrix[count/9][count%9] == i){
-					set.add(new Couple((count/9)/3, (count%9)/3));
-					strings.add(count/9);
-					columns.add(count%9);
-				}
-			}
-			if(set.size() != 9 || strings.size()!= 9 || columns.size() != 9){
-				ok = false;
-				break;
-			}
-		}
-		if(ok)
-			System.out.println("Ok");
-		else
-			System.out.println("!ok");
+		return true;
 	}
 }
